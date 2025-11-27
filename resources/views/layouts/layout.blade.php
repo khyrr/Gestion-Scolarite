e16a38faf329e3d0aa36970852e071c293576150
Mohamed Salem Kh <mohamedsalemkhyarhoum@gmail.com>
2025-11-27 22:37:15 +0100

    chore: update layout css path for moved sidebarStyle.css

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/sidebarStyle.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/legacy/sidebarStyle.css') }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
